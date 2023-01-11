2e85f1f0fa02f1a0cf120b70b2d7d3275e07b877
amoi <alex.amoi@mastercard.com>
2023-01-12 02:13:17 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My First GatsBy Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
